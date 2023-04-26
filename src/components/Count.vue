eeb2b7d9bfc9a9c5b98275e3b67235bd71a93a18
Merite15 <86039889+Merite15@users.noreply.github.com>
2023-04-26 13:46:01 +0100

    add vuejs component

---
+++ src/components/Count.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        {{ count }}
+        <button @click="count++"> Increment</button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const count = ref<number>(0)
+
+</script>
